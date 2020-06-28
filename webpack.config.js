7c2557a24619191a0fd3ec51c2caede4c9102c76
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-06-28 22:05:55 +0100

    simplified webpack and added wds

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,31 @@
-// Webpack uses this to work with directories
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: './src/js/index.js',
-
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
+  entry: { index: path.resolve(__dirname, 'src/js', 'index.js') },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
   },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
-  mode: process.env.NODE_ENV || 'development',
-
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
       {
-        // Apply rule for .sass, .scss or .css files
-        test: /\.(sa|sc|c)ss$/,
-
-        // Set loaders to transform files.
-        // Loaders are applying from right to left(!)
-        // The first loader will be applied after others
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            // This loader resolves url() and @imports inside CSS
-            loader: 'css-loader',
-          },
-          {
-            // Then we apply postCSS fixes like autoprefixer and minifying
-            loader: 'postcss-loader',
-          },
-          {
-            // First we transform SASS to standard CSS
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-            },
-          },
-        ],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        // Now we apply rule for images
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [
-          {
-            // Using file-loader for these files
-            loader: 'file-loader',
-
-            // In options we can set different things like format
-            // and directory to save
-            options: {
-              outputPath: 'images',
-            },
-          },
-        ],
-      },
-      {
-        // Apply rule for fonts files
-        test: /\.(woff|woff2|ttf|otf|eot)$/,
-        use: [
-          {
-            // Using file-loader too
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-            },
-          },
-        ],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'bundle.css',
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
 }
