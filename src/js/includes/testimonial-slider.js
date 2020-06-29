0c50a2a910a4f9ec695d32ad247e447562e7b824
Matthew Horne <mattjhorne@hotmail.co.uk>
2020-06-29 12:52:00 +0100

    initial slider class

--- src/js/includes/testimonial-slider.js
+++ src/js/includes/testimonial-slider.js
@@ -1,3 +1,36 @@
-export function TestimonialSlider() {
-  console.log('I am the slider!')
+export default class TestimonialSlider {
+  constructor(slider, options = {}) {
+    this.slider = slider
+    this.slideData = options.slideData || []
+    this.render()
+  }
+
+  render() {
+    this.slider.insertAdjacentHTML(
+      'beforeend',
+      `<div class="testimonial-slides">
+      ${this.slideData.map(
+        (slide) =>
+          `<div class="testimonial-slide">
+          <div class="testimonial-slide-content">
+            <div class="quote-content">
+              <div class="quote-text">${slide.review}</div>
+              <div class="quote-cite">
+                <span class="quote-author">${slide.author}</span>
+                <span class="quote-occupation">${slide.occupation}</span>
+              </div>
+            </div>
+            <div class="quote-avatar">
+              <img src="${slide.imgSrc}" alt="" />
+            </div>
+          </div>
+        </div>`
+      )}
+      </div>`
+    )
+  }
+
+  nextSlide() {}
+
+  prevSlide() {}
 }
